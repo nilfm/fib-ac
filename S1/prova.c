c955c6ffa4584b14111f2710c761306ea5a5cfa4
nilfm <nil.fons@gmail.com>
2019-09-13 15:50:59 +0200

    Previ1

---
+++ S1/prova.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int pedrito() {
+	return 3;
+}
+
+int main() {
+	printf("%d\n", pedrito());
+}
---
+++ S1/prova.s
@@ -0,0 +1,48 @@
+	.file	"prova.c"
+	.text
+	.globl	pedrito
+	.type	pedrito, @function
+pedrito:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$3, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	pedrito, .-pedrito
+	.section	.rodata
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$0, %eax
+	call	pedrito
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (SUSE Linux) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
