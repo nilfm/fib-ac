cd8e7201b3868feec449d7bda64c9fc105919af3
nilfm <nil.fons@gmail.com>
2019-10-09 17:41:11 +0200

    S4

--- S4/Busca.s
+++ S4/Busca.s
@@ -3,4 +3,43 @@
 	.globl Buscar
 	.type Buscar,@function
 Buscar:
-        # Aqui viene vuestro codigo
+	pushl %ebp
+    movl %esp, %ebp
+    subl $16, %esp
+    
+    movl $-1, -4(%ebp)						# trobat = -1
+    movl $0, -16(%ebp)						# low = 0
+    movl $0, -8(%ebp)						# mid = low = 0
+    movl 24(%ebp), %eax						# eax = N
+    decl %eax								# eax = N-1
+    movl %eax, -12(%ebp)					# high = N-1
+    jmp condicio							# goto condicio
+
+while:
+    pushl 8(%ebp)							# push v
+    pushl 20(%ebp)							
+    pushl 16(%ebp)	
+    pushl 12(%ebp)							# push X
+    leal -8(%ebp), %eax						# eax = &mid
+    pushl %eax								# push &mid
+    leal -12(%ebp), %eax					# eax = &high
+    pushl %eax								# push &high
+    leal -16(%ebp), %eax					# eax = &low
+    pushl %eax								# push &low
+    call BuscarElemento						# BuscarElemento(&low, &high, &mid, X, v)
+    addl $28, %esp							# restore esp
+    movl %eax, -4(%ebp)						# trobat = BuscarElemento(...)
+    cmpl $0, -4(%ebp) 						# if (trobat >= 0) break
+    jge endwhile
+    
+condicio:
+	movl -16(%ebp), %ecx					# ecx = low
+	cmpl -12(%ebp), %ecx					# if (low <= high) goto while
+	jle while
+	
+endwhile:
+	movl -4(%ebp), %eax						# return trobat
+	
+    movl %ebp, %esp
+    popl %ebp
+    ret
--- S4/BuscaElemento.s
+++ S4/BuscaElemento.s
@@ -3,4 +3,40 @@
 	.globl BuscarElemento
 	.type BuscarElemento,@function
 BuscarElemento:
-        # Aqui viene vuestro codigo
+    pushl %ebp
+    movl %esp, %ebp
+    
+    movl 16(%ebp), %ecx                 # ecx = mid
+    movl (%ecx), %ecx                   # ecx = *mid
+    imull $12, %ecx, %ecx               # ecx = *mid*12
+    movl 32(%ebp), %edx                 # edx = @v
+    movl 4(%ecx, %edx), %ecx            # edx = @v+*mid*12+4 = v[*mid].k
+    cmpl 24(%ebp), %ecx                 # if X.k != v[*mid].k goto else 
+    jne else
+if:
+    movl 16(%ebp), %eax                 # eax = mid
+    movl (%eax), %eax                   # eax = *mid
+    jmp endif                           # goto endif, return *mid
+else:
+    movl 16(%ebp), %ecx                 # ecx = mid
+    movl 12(%ebp), %edx                 # edx = high
+    movl (%edx), %edx                   # edx = *high
+    cmpl %edx, (%ecx)                   # if *mid >= *high goto else2
+    jge else2
+if2:
+    movl %edx, (%ecx)                   # *mid = *high
+    movl 8(%ebp), %eax                  # eax = low
+    incl (%eax)                         # (*low)++
+    jmp endif2                          # goto endif2
+else2:
+    movl 8(%ebp), %edx                  # edx = low
+    movl (%edx), %edx                   # edx = *low
+    movl %edx, (%ecx)                   # *mid = *low
+    movl 12(%ebp), %eax                 # eax = high
+    decl (%eax)                         # (*high)--
+endif2:
+    movl $-1, %eax                      # return -1
+endif:
+    movl %ebp, %esp
+    popl %ebp
+    ret
